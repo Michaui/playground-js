455eabaaa10c4013b1b1566286cc777bff2d3316
Michael Franiczek <michael.franiczek@icloud.com>
2023-09-28 12:02:38 +0200

    Add score functions

--- myscript.js
+++ myscript.js
@@ -1,7 +1,6 @@
 
 /*
 1: Choose ‘Rock’, ‘Paper’ or ‘Scissors’
-2: Klick start
 3: Computer choose a random ‘Rock’, ‘Paper’ or ‘Scissors’
 4: Match: IF/ELSE
     Rock > Scissors
@@ -9,6 +8,8 @@
     Siccors > Paper
 5: Log: You win/lose
 */
+
+
 
 function getComputerChoice () {
     //Get a random number between 0 and 2
@@ -23,41 +24,92 @@ function getComputerChoice () {
     }
 };
 
-function playRound(playerSelection, computerSelection) {
-    
-    if (computerSelection == "rock" && playerSelection == "rock" || computerSelection == "paper" && playerSelection == "paper" || computerSelection == "scissor" && playerSelection == "scissor"){
-        return "DRAW! " + playerSelection + " equals " + computerSelection;
-    } else if (computerSelection == "paper" && playerSelection == "rock" || computerSelection == "scissor" && playerSelection == "paper" || computerSelection == "rock" && playerSelection == "scissor"){
-        return "YOU LOSE! " + computerSelection + " beats " + playerSelection; 
-    } 
-    else if(computerSelection == "rock" && playerSelection == "paper" || computerSelection == "paper" && playerSelection == "scissor" || computerSelection == "scissor" && playerSelection == "rock"){
-        return "YOU WIN! " + playerSelection + " beats " + computerSelection;
-    }
-    else if(playerSelection !== "paper" || playerSelection !== "rock" || playerSelection !== "scissor"){
-        return "FUCK YOU! Select a right one!"
-    }
-}
+function playRound(playerSelection, computerSelection, pointP, pointC) {
+
+    if (computerSelection == "rock" && playerSelection == "rock" || 
+            computerSelection == "paper" && playerSelection == "paper" || 
+            computerSelection == "scissor" && playerSelection == "scissor"){
+                return {
+                    pointP: pointP,
+                    pointC: pointC,
+                    text: "DRAW! " + playerSelection + " equals " + computerSelection
+                };
+    } else if (computerSelection == "paper" && playerSelection == "rock" || 
+            computerSelection == "scissor" && playerSelection == "paper" || 
+            computerSelection == "rock" && playerSelection == "scissor"){
+                return {
+                    pointP: pointP,
+                    pointC: pointC + 1,
+                    text: "YOU LOSE! " + computerSelection + " beats " + playerSelection
+                };
+    } else if(computerSelection == "rock" && playerSelection == "paper" || 
+            computerSelection == "paper" && playerSelection == "scissor" || 
+            computerSelection == "scissor" && playerSelection == "rock"){
+                return {
+                    pointP: pointP + 1,
+                    pointC: pointC,
+                    text: "YOU WIN! " + playerSelection + " beats " + computerSelection
+                };
+    } else if(playerSelection !== "paper" || 
+            playerSelection !== "rock" || 
+            playerSelection !== "scissor"){
+                return {
+                    pointP: pointP,
+                    pointC: pointC,
+                    text: "FUCK! Select a right one!"
+                };
+    };
+};
+
+
 
 function game(){
+    // Set default start points
+    /*let playerPoints = 0,
+        computerPoints = 0; */
+
+    /* - Overwritw start variables, because they set back to 0 after each round, because playRound() arguments was defined as (..., playerPoints, computerPoints)
+       - Solution: Declare start points as objetct return  
+                    pointP: playerPoints, TO pointP: pointP,
+                    pointC: computerPoints,TO pointC: pointC,
+    */
+
+    let pointP = 0,
+        pointC = 0; 
+    
     //Define 5 Rounds
     for (i=0; i<5; i++){
-        
+    
         //Loop
-        console.log(i);
-
+        console.log('Loop: ',i);
+        
         //Player choose selection & Save player selection in variable and set all chars to lowerCase. 
-        const playerSelection = prompt("Please select rock, paper or scissors: ").toLowerCase();
-        console.log(playerSelection);
+        let playerSelection = prompt("Please select rock, paper or scissor: ").toLowerCase();
+        console.log('PlayerCoice: ', playerSelection);
 
         //Every loop computer choose a random selection. 
         //save result from getcomputerChoice to computerSelection var.
-        const computerSelection = getComputerChoice();
-
-        //Report winner and loser after one round
+        let computerSelection = getComputerChoice();
         console.log('ComputerChoice:', computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        
+        //Start playRound and set arguments. 
+        //Save the return (return is a object) from playRound in a variable namend decision.
+        //Console.log win/loss after each round with points and text. 
+        let decision = playRound(playerSelection, computerSelection, pointP, pointC);
+        console.log(decision.text + ' Points: Computer ' + decision.pointC,'Player ' + decision.pointP);
+
+        //Place new score back to start variables
+        pointP = decision.pointP;
+        pointC = decision.pointC;
     }
     //Report Winner and score at the end 
-}
+    console.log("Thanks for playing"); 
+
+    if(pointC > pointP){
+        console.log("Sry, you lose! Try again")
+    } else {
+        console.log("Congratulation! You win the game")
+    };
+};
 
 game();
